862fe75d173c9794d08035d360ef0ad6c50220a0
Gourav Bhadra <118394124+gouravbhadra2001@users.noreply.github.com>
2024-03-17 01:31:11 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+import os
+import google.generativeai as genai
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+    
+genai.configure(api_key='AIzaSyCitAX1XE_qmi59IiF0EC0-hbgoHyN_zQI')
+
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
+def get_gemini_response(question):
+    
+    response=chat.send_message(question,stream=True)
+    return response
+#In=input("Enter the text")
+#out= print(get_gemini_response(In))
+
+    
+@app.route("/chatbot/", methods=["POST"])
+def chatbot():
+    data = request.json
+    text = data["text"]
+    response_text = ""
+    for chunk in get_gemini_response(text):
+        response_text += chunk.text
+    return jsonify({"response": response_text})
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
